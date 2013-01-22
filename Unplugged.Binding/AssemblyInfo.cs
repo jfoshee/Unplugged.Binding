ce89692433f80d09741799cb27389f065f47e7d4
Jacob Foshee <jacobf@gmail.com>
2013-01-22 10:54:38 -0600

    Starting up the shield generator

---
+++ Unplugged.Binding/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Unplugged.Binding")]
+[assembly: AssemblyDescription("Convention-based auto-binding")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Unplugged Development")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Unplugged Development 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: CLSCompliant(true)]
